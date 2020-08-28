d00451353deb8336f9dd4582e99165a5e4674c2b
Ellie Lockhart <69021568+EllieLockhart@users.noreply.github.com>
2020-08-28 17:28:30 -0500

    swapfile
    Adding BeautifulSoup functionality

--- scraper1.py
+++ scraper1.py
@@ -1,7 +1,11 @@
 #import basic HTTP handling processes
 import urllib
 from urllib.request import urlopen
+#import scraping libraries
+from bs4 import BeautifulSoup
 
 #import the main page of Polygon
 polygon = urlopen('https://www.polygon.com/')
-print(polygon.read())
+bs = BeautifulSoup(polygon.read(),'html.parser')
+#print the title of the current bs page
+print(bs.title)
